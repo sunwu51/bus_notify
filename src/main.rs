df307839a3a9a382f68570625cc9a3b8c3c5704f
Frank <sunwu51@126.com>
2022-12-15 06:37:07 +0000

    update code run by itself

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 pub mod core;
 pub mod utils;
+use chrono::Timelike;
+use job_scheduler::Job;
+use chrono_tz::Asia::Shanghai;
+use chrono::Utc;
+use job_scheduler::JobScheduler;
 use log::LevelFilter;
 use reqwest::StatusCode;
 use serde_json::json;
 use warp::reply::*;
 use warp::Filter;
 use std::env;
+use std::time::Duration;
+use std::thread;
 
 #[macro_use]
 extern crate serde_derive;
@@ -13,6 +20,11 @@ extern crate serde_derive;
 extern crate lazy_static;
 #[macro_use]
 extern crate log;
+
+
+lazy_static!{
+    static ref PORT : u16 = env::var("port").unwrap_or("10000".to_string()).parse().unwrap();
+}
 
 #[tokio::main]
 async fn main() {
@@ -22,7 +34,7 @@ async fn main() {
     builder.target(Target::Stdout);
     builder.filter_level(LevelFilter::Info);
     builder.init();
-    let port : u16 = env::var("port").unwrap_or("10000".to_string()).parse().unwrap();
+    
 
     let bus_end_point = warp::path!("bus").and(warp::get()).then(|| async move {
         let bus_info = core::service::fetch_and_notify().await;
@@ -38,7 +50,29 @@ async fn main() {
 
     let health_end_point = warp::path!("healthz").map(|| "ok");
 
+    tokio::spawn(cron());
+
     warp::serve(bus_end_point.or(health_end_point))
-        .run(([0, 0, 0, 0], port))
+        .run(([0, 0, 0, 0], *PORT))
         .await;
+}
+
+
+async fn cron() {
+    let mut sched = JobScheduler::new();
+
+    sched.add(Job::new("0 * * * * *".parse().unwrap(), || {
+        let t = Utc::now().with_timezone(&Shanghai);
+        if t.hour() == 8 {
+            let f = core::service::fetch_and_notify();
+            tokio::spawn(async move{
+                let res = f.await.unwrap();
+            });
+        }
+    }));
+
+    loop {
+        sched.tick();
+        thread::sleep(Duration::from_millis(500));
+    }
 }
